d82f9cb385baf432b12b9fea19d3fa5e7185b28d
thecountdown <hellocountdown@gmail.com>
2016-04-11 23:18:05 -0700

    Update scripts.js

--- ipad/scripts.js
+++ ipad/scripts.js
@@ -16,6 +16,35 @@ document.addEventListener('DOMContentLoaded', function () {
   $('#unit').text("");
   $('#value').addClass('value-expanded');
   $('#value').removeClass('value-normal');
+  
+    particleground(document.getElementById('particles-primary'), {
+    dotColor: 'rgba(255, 255, 255, 0.85)',
+    lineColor: 'rgba(255, 255, 255, 0.1)',
+    minSpeedX: 0.3,
+    maxSpeedX: 0.6,
+    minSpeedY: 0.3,
+    maxSpeedY: 0.6,
+    density: 35000, // One particle every n pixels
+    curvedLines: false,
+    proximity: 100, // How close two dots need to be before they join
+    parallaxMultiplier: 7, // Lower the number is more extreme parallax
+    particleRadius: 6, // Dot size
+  });
+
+  particleground(document.getElementById('particles-secondary'), {
+    dotColor: 'rgba(255, 255, 255, 0.25)',
+    lineColor: 'rgba(255, 255, 255, 0.1)',
+    minSpeedX: 0.075,
+    maxSpeedX: 0.15,
+    minSpeedY: 0.075,
+    maxSpeedY: 0.15,
+    density: 20000, // One particle every n pixels
+    curvedLines: false,
+    proximity: 20, // How close two dots need to be before they join
+    parallaxMultiplier: 20, // Lower the number is more extreme parallax
+    particleRadius: 4, // Dot size
+  });
+  
 
   $('#menu-button').click(function() {
     $('#menu').fadeToggle();
